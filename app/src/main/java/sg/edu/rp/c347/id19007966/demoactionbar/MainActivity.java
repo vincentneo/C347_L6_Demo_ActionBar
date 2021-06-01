6c610643bcb02729373ba600742bcac272e350ad
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-06-01 10:44:47 +0800

    Completed Section A

---
+++ app/src/main/java/sg/edu/rp/c347/id19007966/demoactionbar/MainActivity.java
@@ -0,0 +1,21 @@
+package sg.edu.rp.c347.id19007966.demoactionbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
